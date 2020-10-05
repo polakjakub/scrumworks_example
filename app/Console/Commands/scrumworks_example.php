23aca274998fc903b8c957e137f5e0e47dccc7b7
Jakub Polák <polakjakub@gmail.com>
2020-10-05 19:10:39 +0200

    API example

---
+++ app/Console/Commands/scrumworks_example.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+
+class scrumworks_example extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'scrumworks:example';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command will select users from the database and call some API.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $users = DB::select('SELECT * FROM users');
+        
+        echo "Found " . count($users) . " users\n";
+        foreach ($users as $user) {
+            echo "\tCalling API for user {$user->name}({$user->id}) with result...";
+            $query = http_build_query(["userId" => $user->id, "name" => $user->name,]);
+            $response = Http::get("https://example.com/api/dosomething?" . $query);
+            echo 200 == $response->status() ? '    OK' : "NOT OK";
+            echo "\n";
+        }
+    }
+}
